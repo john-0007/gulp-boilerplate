a5ee4eb7709b405942f58c923cfcd96ee9b5c06b
John <Xohnjoseph@gmail.com>
2017-10-16 23:17:42 +0700

    set gulp with webpack

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+
+$("#btn").click(()=>{
+  alert("Here we go");
+  console.log("here we go")
+  console.log("here we go")
+});
---
+++ app/assets/scripts/modules/test.js
@@ -0,0 +1,6 @@
+import $ from "jquery";
+
+$("btn").click(function(){
+  alert("Here we go");
+  alert("Here we go");
+});
